82d023544f71f86eb472ed1f3ba07fc6db6c82a8
Darren Seay <darrenmseay@gmail.com>
2026-03-14 20:58:11 -0700

    Add PWA support

--- src/main.ts
+++ src/main.ts
@@ -2,3 +2,9 @@ import './styles/main.css';
 import { initApp } from './ui/app';
 
 initApp();
+
+if (import.meta.env.PROD && 'serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    void navigator.serviceWorker.register('/sw.js');
+  });
+}
